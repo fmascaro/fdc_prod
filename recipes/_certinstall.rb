4557f742e47b62889825ff39e4510f089c6e3fec
Adam Gerasimchik <adam.gerasimchik@topimagesystems.com>
2016-07-19 11:05:17 -0500

    Removing passwords for certs with private keys, added root and intermediate certificates for Comodo and DigiCert.  Added the TISSSO cert for LMS authentication.

--- recipes/_certinstall.rb
+++ recipes/_certinstall.rb
@@ -6,13 +6,37 @@ windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/MORGANSSO.cer" do
   store_name "ROOT"
 end
 
+windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/MORGANROOT.cer" do
+  store_name "ROOT"
+end
+
+windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/MORGANINT.cer" do
+  store_name "ROOT"
+end
+
+
+windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/DIGICERTROOT.cer" do
+  store_name "ROOT"
+end
+
+windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/DIGICERTINT.cer" do
+  store_name "CA"
+end
+
 windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/xclient_egisticsinc_com.pfx" do
-  pfx_password "password"
   private_key_acl ["EGISTICS\\FDC-PRD-WEB$"]
 end
 
 windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/xclient_egisticsinc_com.pfx" do
-  pfx_password "password"
   private_key_acl ["EGISTICS\\FDC-PRD-WEB$"]
   store_name "ROOT"
+end
+
+windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/TISSSO_COMP.pfx" do
+  private_key_acl ["EGISTICS\\FDC-PRD-WEB$"]
+end
+
+windows_certificate "//DP-ESL-EFS-01/GOLDREP/SSLCERTIFICATES/TISSSO_COMP.pfx" do
+  store_name "ROOT"
+  private_key_acl ["EGISTICS\\FDC-PRD-WEB$"]
 end
